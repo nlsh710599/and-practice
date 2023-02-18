26f71f07ba1737bb59ef2b7fff1525bee4487dba
chihyen <nlsh710599@gmail.com>
2023-02-18 13:35:25 +0800

    implement db function logic

--- internal/database/api.go
+++ internal/database/api.go
@@ -10,6 +10,10 @@ import (
 
 type RDS interface {
 	CreateTable() error
+	CreateNumber(string, string) error
+	ReadNumber(string) string
+	UpdateNumber(string, string) error
+	DeleteNumber(string) error
 }
 
 type postgresClient struct {
@@ -25,4 +29,26 @@ func (pg *postgresClient) CreateTable() error {
 	}
 
 	return nil
+}
+
+func (pg *postgresClient) CreateNumber(name string, value string) error {
+	return pg.client.Create(&model.Number{Name: name, Value: value}).Error
+}
+
+func (pg *postgresClient) ReadNumber(valueName string) string {
+	var res *model.Number
+	if err := pg.client.Model(&model.Number{}).Where("name = ?", valueName).Find(&res).Error; err != nil {
+		return ""
+	}
+	return res.Value
+}
+
+func (pg *postgresClient) UpdateNumber(name string, value string) error {
+	return pg.client.Model(&model.Number{}).
+		Where("name = ?", name).
+		Update("value", value).Error
+}
+
+func (pg *postgresClient) DeleteNumber(name string) error {
+	return pg.client.Where("name =  ?", name).Delete(&model.Number{}).Error
 }

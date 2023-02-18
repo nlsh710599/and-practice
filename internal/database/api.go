d6a4646f689a1516b5310ec07f7d664bfcd384e9
chihyen <nlsh710599@gmail.com>
2023-02-18 14:25:36 +0800

    refine read number method

--- internal/database/api.go
+++ internal/database/api.go
@@ -11,7 +11,7 @@ import (
 type RDS interface {
 	CreateTable() error
 	CreateNumber(string, string) error
-	ReadNumber(string) string
+	ReadNumber([]string) (map[string]string, error)
 	UpdateNumber(string, string) error
 	DeleteNumber(string) error
 }
@@ -35,12 +35,19 @@ func (pg *postgresClient) CreateNumber(name string, value string) error {
 	return pg.client.Create(&model.Number{Name: name, Value: value}).Error
 }
 
-func (pg *postgresClient) ReadNumber(valueName string) string {
-	var res *model.Number
-	if err := pg.client.Model(&model.Number{}).Where("name = ?", valueName).Find(&res).Error; err != nil {
-		return ""
+func (pg *postgresClient) ReadNumber(valueNames []string) (map[string]string, error) {
+	var tmp *[]model.Number
+	numberMap := make(map[string]string)
+
+	if err := pg.client.Where("name IN ?", valueNames).Find(&tmp).Error; err != nil {
+		return nil, err
 	}
-	return res.Value
+
+	for _, x := range *tmp {
+		numberMap[x.Name] = x.Value
+	}
+
+	return numberMap, nil
 }
 
 func (pg *postgresClient) UpdateNumber(name string, value string) error {

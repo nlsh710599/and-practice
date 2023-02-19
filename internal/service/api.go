faaae12942c66af2a547d500f076aecf18f8df80
chihyen <nlsh710599@gmail.com>
2023-02-19 16:12:39 +0800

    implement arithmetic json rpc apis

--- internal/service/api.go
+++ internal/service/api.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"errors"
+	"math/big"
 	"net/http"
 	"strings"
 
@@ -62,17 +63,157 @@ func (ctrl *BigNumberComputationService) Delete(r *http.Request, args *DeleteReq
 }
 
 func (ctrl *BigNumberComputationService) Add(r *http.Request, args *AddRequest, reply *string) error {
+	addend := new(big.Float)
+	summand := new(big.Float)
+	var ok bool
+
+	numMap, err := ctrl.RDS.ReadNumber([]string{args.Addend, args.Summand})
+	if err != nil {
+		return err
+	}
+
+	if utils.IsNumeric(args.Addend) {
+		addend, ok = addend.SetString(args.Addend)
+		if !ok {
+			return err
+		}
+	} else {
+		addend, ok = addend.SetString(numMap[args.Addend])
+		if !ok {
+			return err
+		}
+	}
+
+	if utils.IsNumeric(args.Summand) {
+		summand, ok = summand.SetString(args.Summand)
+		if !ok {
+			return err
+		}
+	} else {
+		summand, ok = summand.SetString(numMap[args.Summand])
+		if !ok {
+			return err
+		}
+	}
+
+	*reply = new(big.Float).Add(addend, summand).String()
+
 	return nil
 }
 
 func (ctrl *BigNumberComputationService) Subtract(r *http.Request, args *SubtractRequest, reply *string) error {
+	subtrahend := new(big.Float)
+	minuend := new(big.Float)
+	var ok bool
+
+	numMap, err := ctrl.RDS.ReadNumber([]string{args.Subtrahend, args.Minuend})
+	if err != nil {
+		return err
+	}
+
+	if utils.IsNumeric(args.Subtrahend) {
+		subtrahend, ok = subtrahend.SetString(args.Subtrahend)
+		if !ok {
+			return err
+		}
+	} else {
+		subtrahend, ok = subtrahend.SetString(numMap[args.Subtrahend])
+		if !ok {
+			return err
+		}
+	}
+
+	if utils.IsNumeric(args.Minuend) {
+		minuend, ok = minuend.SetString(args.Minuend)
+		if !ok {
+			return err
+		}
+	} else {
+		minuend, ok = minuend.SetString(numMap[args.Minuend])
+		if !ok {
+			return err
+		}
+	}
+
+	*reply = new(big.Float).Sub(subtrahend, minuend).String()
+
 	return nil
 }
 
 func (ctrl *BigNumberComputationService) Multiply(r *http.Request, args *MultiplyRequest, reply *string) error {
+	multiplicand := new(big.Float)
+	multiplier := new(big.Float)
+	var ok bool
+
+	numMap, err := ctrl.RDS.ReadNumber([]string{args.Multiplicand, args.Multiplier})
+	if err != nil {
+		return err
+	}
+
+	if utils.IsNumeric(args.Multiplicand) {
+		multiplicand, ok = multiplicand.SetString(args.Multiplicand)
+		if !ok {
+			return err
+		}
+	} else {
+		multiplicand, ok = multiplicand.SetString(numMap[args.Multiplicand])
+		if !ok {
+			return err
+		}
+	}
+
+	if utils.IsNumeric(args.Multiplier) {
+		multiplier, ok = multiplier.SetString(args.Multiplier)
+		if !ok {
+			return err
+		}
+	} else {
+		multiplier, ok = multiplier.SetString(numMap[args.Multiplier])
+		if !ok {
+			return err
+		}
+	}
+
+	*reply = new(big.Float).Mul(multiplicand, multiplier).String()
+
 	return nil
 }
 
 func (ctrl *BigNumberComputationService) Divide(r *http.Request, args *DivideRequest, reply *string) error {
+	dividend := new(big.Float)
+	divisor := new(big.Float)
+	var ok bool
+
+	numMap, err := ctrl.RDS.ReadNumber([]string{args.Dividend, args.Divisor})
+	if err != nil {
+		return err
+	}
+
+	if utils.IsNumeric(args.Dividend) {
+		dividend, ok = dividend.SetString(args.Dividend)
+		if !ok {
+			return err
+		}
+	} else {
+		dividend, ok = dividend.SetString(numMap[args.Dividend])
+		if !ok {
+			return err
+		}
+	}
+
+	if utils.IsNumeric(args.Divisor) {
+		divisor, ok = divisor.SetString(args.Divisor)
+		if !ok {
+			return err
+		}
+	} else {
+		divisor, ok = divisor.SetString(numMap[args.Divisor])
+		if !ok {
+			return err
+		}
+	}
+
+	*reply = new(big.Float).Quo(dividend, divisor).String()
+
 	return nil
 }
